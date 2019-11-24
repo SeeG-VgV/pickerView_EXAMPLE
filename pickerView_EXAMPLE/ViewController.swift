681f0a0e2e6c3f74709c9f83b7c480ea3bff6564
Grigorij <v.grigorij@icloud.com>
2019-11-24 18:58:19 +0000

    1

--- pickerView_EXAMPLE/ViewController.swift
+++ pickerView_EXAMPLE/ViewController.swift
@@ -8,13 +8,35 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet var picker: UIPickerView!
+    
+    var pickerData = ["Item 1", "Item 2", "Item 3", "Item 4", "Item 5", "Item 6"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.picker.delegate = self
+        self.picker.dataSource = self
+        
     }
 
-
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        pickerData.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        pickerData[row]
+    }
+    
+    @IBAction func changePickerView(_ sender: Any) {
+        pickerData = ["Item A", "Item B", "Item C"]
+        // не знаю как изменить
+    }
 }
 
